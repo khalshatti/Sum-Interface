8c1a503750d1fd5a7d74521ff3864901bf6bd7f0
khalshatti <46720593+khalshatti@users.noreply.github.com>
2020-01-25 16:22:07 -0500

    Add files via upload

---
+++ OverloadingSum/OverloadingSum.java
@@ -0,0 +1,328 @@
+//*************************************************************************
+// OverloadingSum.java     Author:Khaled Alshatti
+//
+// to create multiple methods with the same name but different signatures
+//*************************************************************************
+import java.util.Random;
+
+
+//This class overloads the sum method several ways.
+public class OverloadingSum { 
+   public static void main(String[] args) {
+      OverloadingSum s = new OverloadingSum();
+      short a = 1;
+      int b = 2;
+      System.out.println(s.sum(a,b));
+   } 
+   ///////////////////////////////////
+   public int sum(short x, short y) {
+      return x+y;
+   }
+     
+   public int sum(int x, int y) {
+      return x+y;
+   }
+    
+   public float sum(float x, float y) {
+      return x+y;
+   }
+   
+   public double sum(double x, double y) {
+      return x+y;
+   }   
+   
+   ///////////////////////////////////////
+   public int sum(int x, short y) {
+      return x+y;
+   }  
+   public int sum(short x, int y) {
+      return x+y;
+   }  
+   public float sum(float x, short y) {
+      return x+y;
+   }  
+   public float sum(short x, float y) {
+      return x+y;
+   } 
+   public double sum(double x, short y) {
+      return x+y;
+   }  
+   public double sum(short x, double y) {
+      return x+y;
+   }            
+   public float sum(int x, float y) {
+      return x+y;
+   } 
+   public float sum(float x, int y) {
+      return x+y;
+   }
+   public double sum(int x, double y) {
+      return x+y;
+   }  
+   public double sum(double x, int y) {
+      return x+y;
+   }   
+   public double sum(float x, double y) {
+      return x+y;
+   }  
+   public double sum(double x, float y) {
+      return x+y;
+   } 
+  
+   ////////////////////////////////////////   
+   public int sum(short x, short y, short z) {
+      return x+y+z;
+   }
+      
+   public int sum(int x, int y, int z) {
+      return x+y+z;
+   }
+    
+   public float sum(float x, float y, float z) {
+      return x+y+z;
+   }
+   
+   public double sum(double x, double y,double z) {
+      return x+y+z;
+   } 
+   ////////////////////////////// ints and short
+   public int sum(int x, int y, short z) {
+      return x+y+z;
+   }
+    
+   public int sum(short x, int y, int z) {
+      return x+y+z;
+   }
+   
+   public int sum(int x, short y, int z) {
+      return x+y+z;
+   }  
+   
+   public int sum(short x, short y, int z) {
+      return x+y+z;
+   }
+    
+   public int sum(short x, int y, short z) {
+      return x+y+z;
+   }
+   
+   public int sum(int x, short y, short z) {
+      return x+y+z;
+   }  
+   ////////////////////////////// ints and floats
+   public float sum(int x, int y, float z) {
+      return x+y+z;
+   }
+    
+   public float sum(float x, int y, int z) {
+      return x+y+z;
+   }
+   
+   public float sum(int x, float y, int z) {
+      return x+y+z;
+   }  
+   
+   public float sum(float x, float y, int z) {
+      return x+y+z;
+   }
+    
+   public float sum(float x, int y, float z) {
+      return x+y+z;
+   }
+   
+   public float sum(int x, float y, float z) {
+      return x+y+z;
+   }  
+   ////////////////////////////// ints and doubles
+   public double sum(int x, int y, double z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, int y, int z) {
+      return x+y+z;
+   }
+   
+   public double sum(int x, double y, int z) {
+      return x+y+z;
+   }  
+   
+   public double sum(double x, double y, int z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, int y, double z) {
+      return x+y+z;
+   }
+   
+   public double sum(int x, double y, double z) {
+      return x+y+z;
+   }   
+  ////////////////////////////// shorts and floats
+   public float sum(short x, short y, float z) {
+      return x+y+z;
+   }
+    
+   public float sum(float x, short y, short z) {
+      return x+y+z;
+   }
+   
+   public float sum(short x, float y, short z) {
+      return x+y+z;
+   }  
+   
+   public float sum(float x, float y, short z) {
+      return x+y+z;
+   }
+    
+   public float sum(float x, short y, float z) {
+      return x+y+z;
+   }
+   
+   public float sum(short x, float y, float z) {
+      return x+y+z;
+   }  
+   ////////////////////////////// shorts and doubles
+   public double sum(short x, short y, double z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, short y, short z) {
+      return x+y+z;
+   }
+   
+   public double sum(short x, double y, short z) {
+      return x+y+z;
+   }  
+   
+   public double sum(double x, double y, short z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, short y, double z) {
+      return x+y+z;
+   }
+   
+   public double sum(short x, double y, double z) {
+      return x+y+z;
+   }     
+   ////////////////////////////// floats and doubles
+   public double sum(float x, float y, double z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, float y, float z) {
+      return x+y+z;
+   }
+   
+   public double sum(float x, double y, float z) {
+      return x+y+z;
+   }  
+   
+   public double sum(double x, double y, float z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, float y, double z) {
+      return x+y+z;
+   }
+   
+   public double sum(float x, double y, double z) {
+      return x+y+z;
+   }  
+   ////////////////////////////// ints, floats and doubles
+   public double sum(int x, float y, double z) {
+      return x+y+z;
+   }
+   
+   public double sum(int x, double y, float z) {
+      return x+y+z;
+   }  
+    
+   public double sum(float x, int y, double z) {
+      return x+y+z;
+   }
+   
+   public double sum(float x, double y, int z) {
+      return x+y+z;
+   }  
+   
+   public double sum(double x, float y, int z) {
+      return x+y+z;
+   }
+    
+   public double sum(double x, int y, float z) {
+      return x+y+z;
+   }
+  ////////////////////////////// shorts,ints, floats
+   public float sum(int x, float y, short z) {
+      return x+y+z;
+   }
+   
+   public float sum(int x, short y, float z) {
+      return x+y+z;
+   }  
+    
+   public float sum(float x, int y, short z) {
+      return x+y+z;
+   }
+   
+   public float sum(float x, short y, int z) {
+      return x+y+z;
+   }  
+   
+   public float sum(short x, float y, int z) {
+      return x+y+z;
+   }
+    
+   public float sum(short x, int y, float z) {
+      return x+y+z;
+   }   
+  ////////////////////////////// shorts,floats,doubles
+   public double sum(double x, float y, short z) {
+      return x+y+z;
+   }
+   
+   public double sum(double x, short y, float z) {
+      return x+y+z;
+   }  
+    
+   public double sum(float x, double y, short z) {
+      return x+y+z;
+   }
+   
+   public double sum(float x, short y, double z) {
+      return x+y+z;
+   }  
+   
+   public double sum(short x, float y, double z) {
+      return x+y+z;
+   }
+    
+   public double sum(short x, double y, float z) {
+      return x+y+z;
+   }   
+  ////////////////////////////// shorts,ints, double
+   public double sum(int x, double y, short z) {
+      return x+y+z;
+   }
+   
+   public double sum(int x, short y, double z) {
+      return x+y+z;
+   }  
+    
+   public double sum(double x, int y, short z) {
+      return x+y+z;
+   }
+   
+   public double sum(double x, short y, int z) {
+      return x+y+z;
+   }  
+   
+   public double sum(short x, double y, int z) {
+      return x+y+z;
+   }
+    
+   public double sum(short x, int y, double z) {
+      return x+y+z;
+   }                              
+}
